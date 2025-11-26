ea55fecb4110f065a3e663a3f1770fac804941ac
fahim26-maker <fmuntasir185@gmail.com>
2025-11-26 20:43:50 +0600

    janina

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -4,10 +4,11 @@ import Home from "../pages/Home/Home";
 import ToyDetails from "../pages/toyDetails/ToyDetails";
 import Login from "../pages/Login/Login";
 import Register from "../pages/Register/Register";
-import ErrorElement from "../pages/ErrorElement/Errorelement";
 import Auth from "../pages/Auth/Auth";
 import AuthLayout from "../layouts/AuthLayout/AuthLayout";
 import PrivateRoute from "../provider/PrivateRoute";
+import Loading from "../pages/loading/Loading";
+import ErrorElement from "../pages/ErrorElement/Errorelement";
 
 
 const router = createBrowserRouter([
@@ -20,6 +21,7 @@ const router = createBrowserRouter([
         index: true,
         element: <Home></Home>,
         loader: () => fetch("/cars.json"),
+        hydrateFallbackElement: <Loading></Loading>,
       },
       {
         path: "my-profile",
@@ -28,6 +30,7 @@ const router = createBrowserRouter([
       {
         path: "toyDetails/:id",
         loader: () => fetch("/cars.json"),
+        hydrateFallbackElement: <Loading></Loading>,
         element: <PrivateRoute>
           <ToyDetails></ToyDetails>
         </PrivateRoute>,
--- src/pages/ErrorElement/ErrorElement.jsx
+++ src/pages/ErrorElement/ErrorElement.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Link, useRouteError } from 'react-router';
+import lota from "../../assets/Group.png"
+import er from "../../assets/Group (2).png"
 
 const ErrorElement = () => {
+     const error  = useRouteError()
+    console.log(error)
     return (
-        <div>
-            error found
+        <div class="bg-[#FFF0E1] h-screen">
+
+
+        <div className=' justify-center items-center justify-items-center pt-20'>       
+
+            <img src={lota} className=' absolute ' />
+            <img src={er} className=' relative top-20' />
+        </div>
+        <p className='text-center text-4xl font-bold mt-30'>Oops, page not found!</p>
+        <p className='text-center text-gray-500 mt-4'>The page you are looking for is not available.</p>
+        <Link to={"/"}><button className='bg-gradient-to-r from-[#632EE3] to-[#9F62F2] text-white btn  mx-auto flex justify-center mt-4'>Go Back !!</button></Link>
+
         </div>
     );
 };
